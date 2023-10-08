1447ab635a47ed522560b5100c6ef2586fac158d
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-10-08 12:57:37 +0400

    add routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
+// default
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+// Pages
+import Home from '@/pages/Home'
+import NotFound from '@/pages/404'
+import Users from '@/pages/Users'
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+// Routering
+export default new Router({
+  // mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      component: NotFound
+    }
+  ]
 })
-
-export default router
