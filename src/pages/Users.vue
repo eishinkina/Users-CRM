83a0f3ce853c0462309eae05b6f34472781867a6
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-10-12 11:06:10 +0400

    pagination and sorting implemented

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -7,9 +7,9 @@
           <!-- head  -->
           <thead>
             <tr>
-              <th @click="sort('name')">Name</th>
-              <th @click="sort('username')">Username</th>
-              <th @click="sort('city')">City</th>
+              <th @click="sort('name')">Name &#8595;</th>
+              <th @click="sort('username')">Username &#8595;</th>
+              <th @click="sort('city')">City &#8595;</th>
             </tr>
           </thead>
           <!-- body  -->
@@ -29,7 +29,20 @@
             </tr>
           </tbody>
         </table>
-        <p>debug: sort: {{ currentSort }}, dir: {{ currentSortDir }}</p>
+        <p style="text-align: center">
+          <span>debug: sort: {{ currentSort }}, dir: {{ currentSortDir }}</span>
+          <span>
+            page: {{ this.page.current }}, lenght: {{ this.page.length }}
+          </span>
+        </p>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="button-list">
+          <div class="btn btnPrimary" @click="prevPage">&#8592;</div>
+          <div class="btn btnPrimary" @click="nextPage">&#8594;</div>
+        </div>
       </div>
     </section>
   </div>
@@ -43,6 +56,10 @@ export default {
       users: [],
       currentSort: "name",
       currentSortDir: "asc",
+      page: {
+        current: 1,
+        length: 3,
+      },
     };
   },
   created() {
@@ -57,31 +74,50 @@ export default {
   },
   computed: {
     sortedUsers() {
-      return [...this.users].sort((a, b) => {
-        let aValue = a[this.currentSort];
-        let bValue = b[this.currentSort];
+      return [...this.users]
+        .sort((a, b) => {
+          let aValue = a[this.currentSort];
+          let bValue = b[this.currentSort];
 
-        if (this.currentSort === 'city') {
-          aValue = a.address[this.currentSort];
-          bValue = b.address[this.currentSort];
-        }
+          if (this.currentSort === "city") {
+            aValue = a.address[this.currentSort];
+            bValue = b.address[this.currentSort];
+          }
 
-        let mod = 1;
-        if (this.currentSortDir === 'desc') mod = -1;
-        if (aValue < bValue) return -1 * mod;
-        if (aValue > bValue) return 1 * mod;
-        return 0;
-      });
-    }
+          let mod = 1;
+          if (this.currentSortDir === "desc") mod = -1;
+          if (aValue < bValue) return -1 * mod;
+          if (aValue > bValue) return 1 * mod;
+          return 0;
+        })
+        .filter((row, index) => {
+          let start = (this.page.current - 1) * this.page.length;
+          let end = this.page.current * this.page.length;
+          if (index >= start && index < end) {
+            return true;
+          }
+        });
+    },
   },
   methods: {
     sort(e) {
       if (e === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
       }
       this.currentSort = e;
-    }
-  }
+    },
+    //pagination
+    nextPage() {
+      if (this.page.current * this.page.length < this.users.length) {
+        this.page.current++;
+      }
+    },
+    prevPage() {
+      if (this.page.current > 1) {
+        this.page.current--;
+      }
+    },
+  },
 };
 </script>
 
@@ -91,5 +127,13 @@ img {
   height: auto;
   border-radius: 50%;
   margin-right: 16px;
+}
+.button-list {
+  width: 100%;
+  text-align: center;
+  .btn {
+    border-radius: 60px;
+    margin: 0 20px;
+  }
 }
 </style>
